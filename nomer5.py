02435d3ce210c917e227510036715c9d3ec1cd76
sanechkadap <159237718+sanechkadap@users.noreply.github.com>
2024-02-07 11:22:36 +0300

    Create nomer5.py

---
+++ nomer5.py
@@ -0,0 +1,13 @@
+import math
+
+N = int(input('Vvedite N:'))
+A = int(input('Vvedite A:'))
+B = int(input('Vvedite B:'))
+C = int(input('Vvedite C:'))
+
+D = N-(A+B+C)
+print("Kol-vo Troeshnikov:")
+print(D)
+
+I = D/N*100
+print(I)
