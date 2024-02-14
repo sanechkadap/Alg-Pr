25e0cea16990863bba05728b12c51b939b43c24f
sanechkadap <159237718+sanechkadap@users.noreply.github.com>
2024-02-14 11:33:44 +0300

    Update nomer10.py

--- nomer10.py
+++ nomer10.py
@@ -1,9 +1,9 @@
 import math
 
- R = int(input("R:"))
- r = int(input("r:"))
- h = int(input("h:"))
- l = math.sqrt(math.pow(h,2)+(math.pow((R-r),2)))
- v = (1/3)*math.pi(math.pow(R,2)+math.pow(r,2)+R*r)
- s = math.pi*(R+r)*l+math.pi*math.pow(R,2)+math.pi*math.pow(r,2)
- print(V,S)
+R = int(input("R:"))
+r = int(input("r:"))
+h = int(input("h:"))
+l = math.sqrt(math.pow(h,2)+(math.pow((R-r),2)))
+v = (1/3)*math.pi(math.pow(R,2)+math.pow(r,2)+R*r)
+s = math.pi*(R+r)*l+math.pi*math.pow(R,2)+math.pi*math.pow(r,2)
+print(V,S)
