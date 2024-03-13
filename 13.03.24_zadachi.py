10872a15aa8b747f912576a30dfa04820c1ef37b
sanechkadap <159237718+sanechkadap@users.noreply.github.com>
2024-03-13 11:34:19 +0300

    Create 13.03.24_zadachi.py

---
+++ 13.03.24_zadachi.py
@@ -0,0 +1,19 @@
+import math
+n = -100
+while (3*n*math.pow(n, 4)-730*n<5):
+    n+=1
+print(n)
+##############################
+import math
+n = 2
+while (math.pow(math.e ,n) - 1000 * math.log(10,n)<=10):
+    n+=1
+print(n)
+#################################
+import math
+m = int(input(("Введи число: ")))
+k = 1
+while pow(4 , k) < m :
+    k+=1
+    print(k)
+###############################
